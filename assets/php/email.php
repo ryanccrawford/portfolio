0accb5e36769bd4f19c00d6780fba41d960c02f1
RyBo <ryanccrawford@live.com>
2019-05-13 06:29:16 -0400

    Updated

--- assets/php/email.php
+++ assets/php/email.php
@@ -1,8 +1,8 @@
 <?php
 mb_internal_encoding("UTF-8");
 
-$to = 'hello@example.com';
-$subject = 'Message from Cryptex';
+$to = 'ryanccrawford@live.com';
+$subject = 'Message from Your Contact Page';
 
 $name = "";
 $email = "";
